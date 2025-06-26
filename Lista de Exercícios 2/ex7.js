9af6318de39eec149e23102bf04f28c71a320d38
CiriloDesu <sirilo-aju-se@hotmail.com>
2025-06-26 18:34:01 -0300

    a

--- Lista de Exercícios 2/ex7.js
+++
@@ -1,5 +0,0 @@
-function mapearOrdenar() {
-    const arr = JSON.parse(document.getElementById('produtos').value);
-    const r = arr.sort((a, b) => a.preco - b.preco).map(p => p.nome);
-    document.getElementById('resultado').innerText = JSON.stringify(r);
-}
